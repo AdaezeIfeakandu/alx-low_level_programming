c8dcba8c3a278a38c10d68f8ee94ec9e43f83cd1
AdaezeIfeakandu <ifeakanduada11@gmail.com>
2022-09-23 09:05:17 -0700

    file 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly "and that piece of art is useful"
+ * main - Prints out a string to stdout
  * Return: Always 0 (Success)
  */
 int main(void)
