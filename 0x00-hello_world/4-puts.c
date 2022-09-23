d301cecaade097b7ff7f861a63b3f1440cd41047
AdaezeIfeakandu <ifeakanduada11@gmail.com>
2022-09-23 07:19:41 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Author: AdaezeIfeakandu
- *
+ * Description: main - Prints out a string to stdout
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char string[70] = "\"Programming is like building a multilingual puzzle\"";
-
-	puts(string);
-
-	/* Tried printing it directly */
-
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
