d2a8653d08eeecbbd71afade8534fdd8031ab1c4
AdaezeIfeakandu <ifeakanduada11@gmail.com>
2022-09-23 07:02:25 -0700

    corrected file 1 and did file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Author: AdaezeIfeakandu
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string[70] = "\"Programming is like building a multilingual puzzle\"";
+
+	puts(string);
+
+	/* Tried printing it directly */
+
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
+}
